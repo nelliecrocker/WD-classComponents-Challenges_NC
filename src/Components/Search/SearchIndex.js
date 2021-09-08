5ff6e09f67dddd850c61e097a5cf3880f6a2ed64
Nellie <80473940+nelliecrocker@users.noreply.github.com>
2021-09-08 19:54:24 -0400

    search component partially working

--- src/Components/Search/SearchIndex.js
+++ src/Components/Search/SearchIndex.js
@@ -1,47 +1,59 @@
-// import React, { Component } from 'react';
-// import { Input } from 'reactstrap';
-// import Search from './Search';
+import React, { Component } from 'react';
+import { Input } from 'reactstrap';
+import Search from './Search';
 
-// class SearchIndex extends Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       things: ['pen', 'marker', 'eraser', 'notebook', 'pencil', 'scissors', 'highlighter', 'stapler', 'paper clip', 'binder', 'hole punch', 'laminator', 'laminating sheets', 'protective sheets', 'index cards']
+class SearchIndex extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      things: ['pen', 'marker', 'eraser', 'notebook', 'pencil', 'scissors', 'highlighter', 'stapler', 'paper clip', 'binder', 'hole punch', 'laminator', 'laminating sheets', 'protective sheets', 'index cards'],
+      searchTerm: ""
+    }
+    this.handleChange.bind(this)
+    this.handleSubmit.bind(this)
+  }
 
-//     }
-//   }
+  searchFunction() {
 
-//   searchFunction(things) {
+    return (
+      <div>
+        {/* needs to be .filter over the input value
+        {this.state.things.map((thing) => { return <p>{thing} </p> })} */}
+      </div>
+    )
+  }
 
-//     return (
-//       <div>
-//         {/* needs to be .filter over the input value */}
-//         {this.state.things.map((thing) => { return <p>{thing} </p> })}
-//       </div>
-//     )
-//   }
+  handleChange(e) {
+    console.log(e.target.value);
 
-//   //needs to bind
-//   // handleChange(e) {
-//   //   console.log(e.target.value);
+  }
 
-//   // }
-
-//   render() {
-//     return (
-//       <div>
-//         <Input placeholder='Search Here'
-//           onChange={(handleChange)}
-//         />
-//         {/* need onchange and onsubmit functions */}
+  handleSubmit(e) {
+    //take search term and add it to state
 
 
-//         <h3>Results:</h3>
-//         { this.searchFunction()}
-//       </div>
-//     )
-//   }
-// }
+    //filter through the things array for the searchTerm
 
-// export default SearchIndex
+
+    //display matches
+
+
+  }
+
+  render() {
+    return (
+      <div>
+        <Input placeholder='Search for Office Supplies'
+          onChange={(this.handleChange)}
+          onSubmit={(this.handleSubmit)}
+        />
+        <br />
+        <h3>Results:</h3>
+        {this.searchFunction()}
+      </div>
+    )
+  }
+}
+
+export default SearchIndex
 
